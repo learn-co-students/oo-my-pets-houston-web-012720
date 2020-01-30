e755ff0b71ffc9715d6906f7fe144f045313dfe3
SteRobWms <stephen.r.williams.87@gmail.com>
2020-01-30 16:04:34 -0600

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class Cat
-  # code goes here
+
+  @@all =[]
+
+  attr_accessor :owner, :mood
+  attr_reader :name, :species
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @species = "cat"
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
+
+# Cat.new("snoopy", "shelter")
+# Cat.new("pluto", "shelter")
+# Cat.new("goofy", "shelter")
+
+# binding.pry
+0
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class Dog
-  # code goes here
+
+  @@all=[]
+
+  attr_accessor :owner, :mood
+  attr_reader :name, :species
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @species = "dog"
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
+
+# Dog.new("garfield", "shelter")
+# Dog.new("otis", "shelter")
+# Dog.new("tom", "shelter")
+
+# binding.pry
+0
