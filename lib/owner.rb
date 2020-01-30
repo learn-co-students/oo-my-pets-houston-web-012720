3eba0f9e836b683ba536c741ae2e7eb73440b797
timothyalton <mitchell.alton@live.com>
2020-01-30 14:50:10 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  # attr_accessor  :cat, :dog
+  @@all = [] 
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a human."
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"} 
+  end
+
+  def feed_cats 
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.each {|cat| cat.mood = "nervous"}
+    self.dogs.each {|dog| dog.mood = "nervous"}
+    self.cats.each {|cat| cat.owner = nil}
+    self.dogs.each {|dog| dog.owner = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  
 end
+
+# o1 = Owner.new("Jeff")
+
+# binding.pry
+# 0
