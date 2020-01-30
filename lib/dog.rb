22eb4fcde317d8666e87fa23d5322fb593b2d942
vienpham2019 <vienpham2019@gmail.com>
2020-01-30 15:30:16 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :mood 
+  attr_reader :type
+  @@all = []
+  def initialize(name,owner = nil)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    @type = "dog"
+    @@all << self
+  end
+
+  def name 
+    @name 
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.count
+    @@all.length
+  end
 end
+
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
+require "pry"
 class Owner
   # code goes here
+  attr_reader :name , :species
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    @species = "I am a human."
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.count 
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.clear
+    @@all = []
+  end
+
+  def cats
+    Cat.all.select{|e| e.owner == self} 
+  end
+
+  def dogs
+    Dog.all.select{|e| e.owner == self} 
+  end
+  
+  def buy_cat(name)
+    # binding.pry
+    cats << Cat.new(name,self)
+  end
+
+  def buy_dog(name)
+    dogs << Dog.new(name,self)
+  end
+
+  def walk_dogs
+    dogs.each{|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    cats.each{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    [dogs,cats].flatten.each do |a|
+      a.mood = "nervous"
+      a.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+  end
 end
