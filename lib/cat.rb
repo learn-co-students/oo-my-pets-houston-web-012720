d636da0429ac065e71ae40c2aa560e0595db354e
DrVonDevious <drvondevious@gmail.com>
2020-01-30 13:34:17 -0600

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @owner.cats << self
+  end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
+  end  
+
 end
